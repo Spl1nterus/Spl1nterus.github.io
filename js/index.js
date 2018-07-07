109cca3736c29e8df8c850bd270e46aa06ce837c
Alexey Malyshev <40164656+Spl1nterus@users.noreply.github.com>
2018-07-07 21:16:42 +0300

    oprimized

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $('.product-popup--close').on('click', function(e) {
   $('.product-card').removeClass('product-card--hide');
 	 $('.product-form').removeClass('product-form--show');
 });
-$('.form__button').on('click', function(e) {
+$('.js__button').on('click', function(e) {
   e.preventDefault;
   $('.overlay').show();
   $('.popup').show();
