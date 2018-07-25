9f2baccedd581fed47fbbdc52a0607414dd7564a
Alexey Malyshev <40164656+Spl1nterus@users.noreply.github.com>
2018-07-25 16:25:16 +0300

    Commit

--- sinapsTest/js/index.js
+++ sinapsTest/js/index.js
@@ -1,7 +1,7 @@
 $('.page-header__btn').on('click', function(e) {
   e.preventDefault();
   $(this).toggleClass('page-header__btn--active');
-  $('.main-nav__list').toggleClass('main-nav__list--active');
+  $('.header-nav__list').toggleClass('header-nav__list--active');
   $('.page-header').toggleClass('page-header--active');
 });
 
