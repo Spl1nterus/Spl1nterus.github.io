21320d8eb664213eeb35d7388465d9674d5b63ba
Alexey Malyshev <40164656+Spl1nterus@users.noreply.github.com>
2018-07-26 10:48:45 +0300

    Com

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
+$(document).ready(function(){
 $('.page-header__btn').on('click', function(e) {
   e.preventDefault();
   $(this).toggleClass('page-header__btn--active');
   $('.header-nav__list').toggleClass('header-nav__list--active');
   $('.page-header').toggleClass('page-header--active');
 });
+});
 
-
-if ( matchMedia('screen and (min-width: 1300px)').matches ) {
 var options = {
   prevNextButtons: false,
-
 };
+if ( matchMedia('screen and (min-width: 1330px)').matches ) {
   options.prevNextButtons = true;
   options.asNavFor = ".carousel-main";
   options.contain = true;
@@ -18,6 +18,7 @@ var options = {
   options.draggable = false;
   $('.carousel-nav').flickity( options ); 
 }
+
 
 
 
