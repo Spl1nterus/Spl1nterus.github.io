9a6de5743ba7364f317586572bd7a7f05a21802c
Alexey Malyshev <40164656+Spl1nterus@users.noreply.github.com>
2018-08-12 14:18:57 +0300

    portfolio

---
+++ HYPE/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){	
+$('.product-slider').slick({
+  infinite: true,
+  initialSlide: 1,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: "0px",
+   });
+
+});
+
+
+		
