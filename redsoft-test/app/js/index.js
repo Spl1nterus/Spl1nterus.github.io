37041fa860c6b1937d7c973a013cbd21f8429d3d
Alexey Malyshev <40164656+Spl1nterus@users.noreply.github.com>
2019-01-27 17:05:49 +0300

    redsoft-test

---
+++ redsoft-test/app/js/index.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+      $('.header-nav__btn').on('click', function (e) {
+            e.preventDefault();
+        e.preventDefault();
+        $('.header-nav__list').toggleClass('header-nav__list--active');
+      });
+
+     });
