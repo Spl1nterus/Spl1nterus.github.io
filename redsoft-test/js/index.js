dae01a11c28d9204df81a5ea4de34567c6fc13ec
Alexey Malyshev <40164656+Spl1nterus@users.noreply.github.com>
2019-01-27 17:50:11 +0300

    ,

--- redsoft-test/js/index.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function () {
-      $('.header-nav__btn').on('click', function (e) {
-            e.preventDefault();
-        e.preventDefault();
-        $('.header-nav__list').toggleClass('header-nav__list--active');
-      });
-
-     });
